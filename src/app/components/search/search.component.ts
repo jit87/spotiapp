8b6a0fe1a2c7ff1c2673667a31ff67781c214f60
Ignacio <ignaciotejada87@gmail.com>
2024-03-12 14:57:14 +0100

    Upload 12/03/2024 1

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-search',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
+
+  artistas: any[] = [];
+
+  constructor(private spotify: SpotifyService){}
+
+  buscar(termino: string){
+    console.log(termino);
+    this.spotify.getArtista(termino)
+        .subscribe( (data:any) => {
+            this.artistas = data; 
+        });
+  }
 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 
 
 @Injectable({
@@ -16,15 +18,38 @@ export class SpotifyService {
   getNewReleases(): Observable<any>{
 
     //Token obtenido con Postman siguiendo instrucciones de https://developer.spotify.com/documentation/web-api/tutorials/client-credentials-flow
+    //El token caduca a la hora, para renovarlo hay que utilizar Postman
     const headers = new HttpHeaders({
-      'Authorization' : 'Bearer BQD2Bs5Rbgd3WhFaVoYYuQKIMxu3lHBD1gSoZyohf96XD35pp3G4Ud5NrzGL-PkL8WllKWffzQaJYiYpeTZ3ojafpCJAlFhh64nQD2AA1u03Fy4TRM8'
+      'Authorization' : 'Bearer BQB9xrkCjVFQchXvZA_JMBQilMLvoliwk8jGues0PsM7kgqSOox-nZGcG7dagz8uyLyn1sVOjvq8XU9mIAPVSEhX4222dDv_n556G7w3ZjV9A_15v-Q'
     });
 
-    return this.http.get( 'https://api.spotify.com/v1/browse/new-releases',{ headers });
+    return this.http.get( 'https://api.spotify.com/v1/browse/new-releases',{ headers })
+                    .pipe( map( (data:any) => {
+                        return data.albums.items; 
+                    }) );
+
+  }
+
+
+  getArtista( termino: string ){
+
+    const headers = new HttpHeaders({
+      'Authorization' : 'Bearer BQB9xrkCjVFQchXvZA_JMBQilMLvoliwk8jGues0PsM7kgqSOox-nZGcG7dagz8uyLyn1sVOjvq8XU9mIAPVSEhX4222dDv_n556G7w3ZjV9A_15v-Q'
+    });
+
+    return this.http.get( `https://api.spotify.com/v1/search?q=${termino}&type=artist`,{ headers })
+                    .pipe( map( (data:any) => {
+                      return data.artists.items; 
+                  }) );
 
   }
 
 
 
 
-}
+  }
+
+
+
+
+
